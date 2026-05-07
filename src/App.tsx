248314a42783772805351895f7eda565168e8ea5
Marta Grau Garriga <marta.grau@alu.ceacfp.es>
2026-05-07 13:46:21 +0200

    Mejoras

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { FinanceStatusBar } from './components/FinanceStatusBar'
 import { FinanceProvider } from './context/FinanceProvider'
 import { AppLayout } from './layouts/AppLayout'
 import { EditTransactionPage } from './pages/EditTransactionPage'
@@ -11,7 +10,6 @@ function App() {
   return (
     <BrowserRouter>
       <FinanceProvider>
-        <FinanceStatusBar />
         <Routes>
           <Route element={<AppLayout />}>
             <Route index element={<TransfersHomePage />} />
